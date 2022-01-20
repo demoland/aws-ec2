93b4f77e7ac670df6f49dd64bc181d669938806e
danfedick <daniel.fedick@hashicorp.com>
2022-01-20 15:14:20 -0500

    Add consul security group

--- sg.tf
+++ sg.tf
@@ -23,3 +23,29 @@ resource "aws_security_group" "demoland_ssh" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_security_group" "demoland_consul" {
+  name        = "demoland_ssh"
+  description = "Allow Consul inbound traffic"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    cidr_blocks = [local.cidr_block, local.my_ip]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_consul"
+  }
+}
